47c7cf20ee903e3ed8c4db26ddade785a37b5abc
Mainak <developer.mainakbanerjee@gmail.com>
2023-04-06 15:20:30 +0530

    fix: updated code to get data from json server

--- pages/reviews/[slug].jsx
+++ pages/reviews/[slug].jsx
@@ -1,35 +1,34 @@
-import React from 'react'
+import React from "react";
 import fs from "fs";
 
-const Review = ({movieDetails}) => {
-  return (
-    <div>{movieDetails?.title}</div>
-  )
-}
+const Review = ({ movieDetails }) => {
+  return <div>{movieDetails?.title}</div>;
+};
 
-export default Review
+export default Review;
 
 export const getStaticPaths = async () => {
-    const files = fs.readdirSync("data");
-    const paths = files.map(item => {
-        const pathName = item.split('.')
-        return {
-            params: {slug: pathName[0]}
-        }
-    })
+  const files = fs.readdirSync("data");
+  const paths = files.map((item) => {
+    const pathName = item.split(".");
     return {
-        paths,
-        fallback: false
-    }
-}
+      params: { slug: pathName[0] },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
 export const getStaticProps = async (context) => {
-    const fileName = context.params.slug
-    const movieDetails =JSON.parse(fs.readFileSync(`data/${fileName}.json`))
+  const movieName = context.params.slug;
+  const res = await fetch(`http://localhost:4000/hindiMovies/${movieName}`);
+  const movieDetails = await res.json();
 
-    return {
-        props: {
-            movieDetails
-        }
-    }
-}
+  return {
+    props: {
+      movieDetails,
+    },
+  };
+};
--- pages/reviews/index.jsx
+++ pages/reviews/index.jsx
@@ -1,31 +1,25 @@
 import React from "react";
-import fs from "fs";
 
-
-const Reviews = ({moviesData}) => {
-  return <div>
-    <h2>Reviews</h2>
-    {moviesData.map(movie => {
-        return (<p key={movie.id}>{movie.title}</p>)
-    })
-
-    }
-  </div>;
+const Reviews = ({ moviesData }) => {
+  return (
+    <div>
+      <h2>Reviews</h2>
+      {moviesData.map((movie) => {
+        return <p key={movie.id}>{movie.title}</p>;
+      })}
+    </div>
+  );
 };
 
 export default Reviews;
 
 export const getStaticProps = async () => {
-  const files = fs.readdirSync("data");
-  const moviesData = files.map((file) => {
-    const content = fs.readFileSync(`data/${file}`, "utf-8");
-    const movieData = JSON.parse(content);
-    return movieData;
-  });
+  const res = await fetch("http://localhost:4000/hindiMovies");
+  const moviesData = await res?.json();
 
   return {
     props: {
-        moviesData
-    }
-  }
+      moviesData,
+    },
+  };
 };
